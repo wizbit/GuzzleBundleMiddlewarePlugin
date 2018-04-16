799a50df9a959f83b9057ff23f2822a82a505808
Marcus Speight <marcus@pmconnect.co.uk>
2018-04-16 08:31:13 +0100

    Add middleware to handler

--- src/GuzzleBundleMiddlewarePlugin.php
+++ src/GuzzleBundleMiddlewarePlugin.php
@@ -6,6 +6,8 @@ use EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundlePlugin;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class GuzzleBundleMiddlewarePlugin extends Bundle implements EightPointsGuzzleBundlePlugin
@@ -28,8 +30,26 @@ class GuzzleBundleMiddlewarePlugin extends Bundle implements EightPointsGuzzleBu
     public function addConfiguration(ArrayNodeDefinition $pluginNode)
     {
         $pluginNode
+            ->beforeNormalization()
+            ->ifNotInArray(['enabled'])
+                ->then(function($value) {
+                    return array('middleware' => $value);
+                })
+            ->end()
             ->canBeEnabled()
             ->children()
+                ->arrayNode('middleware')
+                    ->isRequired()
+                    ->requiresAtLeastOneElement()
+                    ->variablePrototype()
+                        ->validate()
+                            ->ifTrue(function ($v) {
+                                return false === is_string($v) && (false === is_array($v) || count($v) !== 2);
+                            })
+                            ->thenInvalid('You must specify a callable')
+                        ->end()
+                    ->end()
+                ->end()
             ->end()
         ;
     }
@@ -59,7 +79,47 @@ class GuzzleBundleMiddlewarePlugin extends Bundle implements EightPointsGuzzleBu
     public function loadForClient(array $config, ContainerBuilder $container, string $clientName, Definition $handler)
     {
         if (true === $config['enabled']) {
-//            $handler->addMethodCall('push', [$forwardHeaderMiddlewareExpression, $this->getPluginName()]);
+            foreach ($config['middleware'] as $middleware) {
+                $handler->addMethodCall('push', $this->parseCallable($middleware));
+            }
         }
+    }
+
+    /**
+     * Parses a callable.
+     *
+     * @param string|array $callable A callable
+     *
+     * @return string|array A parsed callable
+     */
+    private function parseCallable($callable)
+    {
+        if (is_string($callable)) {
+            if ('' !== $callable && '@' === $callable[0]) {
+                throw new InvalidArgumentException(sprintf('The value must be the id of the service without the "@" prefix (replace "%s" with "%s").', $callable, substr($callable, 1)));
+            }
+
+            if (false !== strpos($callable, ':')) {
+
+                if (false === strpos($callable, '::'))  {
+                    $parts = explode(':', $callable);
+                    return array(new Reference($parts[0]), $parts[1]);
+                } else {
+                    return $callable;
+                }
+            }
+
+            return array(new Reference($callable), '__invoke');
+        }
+
+        if (is_array($callable)) {
+            if (isset($callable[0]) && isset($callable[1])) {
+                return array(new Reference($callable[0]), $callable[1]);
+            }
+
+            throw new InvalidArgumentException(sprintf('Middleware must contain an array with two elements. Check your YAML syntax.'));
+        }
+
+        throw new InvalidArgumentException(sprintf('Middleware must be a string or an array. Check your YAML syntax.'));
     }
 }
